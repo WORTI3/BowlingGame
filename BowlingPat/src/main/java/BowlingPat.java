8db9a9bfec5f77bbd8eb3757d2947149d10be290
wortie <ARCWO@NHSBSAADMINs-MacBook-Pro.local>
2020-10-08 11:00:53 +0100

    Updates

--- BowlingPat/src/main/java/BowlingPat.java
+++ BowlingPat/src/main/java/BowlingPat.java
@@ -1,10 +1,14 @@
 public class BowlingPat {
 
-    private int roll = 0;
-    private int[] rolls = new int[21];
+    private final static int MAX_PINS_PER_FRAME = 10;
+    private final static int MAX_FRAMES_PER_GAME = 10;
+    private final static int MAX_BOWLS_PER_GAME = 21;
+
+    private int rollCounter = 0;
+    private final int[] rolls = new int[MAX_BOWLS_PER_GAME];
 
     public void roll(int pins){ // takes the number of pins per roll
-        rolls[roll++] = pins; // assigns the number of pins per roll to rolls foreach roll 0-21
+        rolls[rollCounter++] = pins; // assigns the number of pins per roll to rolls foreach roll 0-21
     }
 
     public int getScore(){
@@ -14,15 +18,15 @@ public class BowlingPat {
         int frame = 0; // Local frame that a game is on up to 10
 
         // counts up through variables until frames or bowls limit is reached
-        while(frame < 10) { // Ten is max amount of frames foreach frame until 10 is reached
-            if(rolls[bowls] == 10){ // Strike if a single frame is equal to ten
-                score += 10 + rolls[bowls + 1] + rolls[bowls + 2];
+        while(frame < MAX_FRAMES_PER_GAME) { // Ten is max amount of frames foreach frame until 10 is reached
+            if(rolls[bowls] == MAX_PINS_PER_FRAME){ // Strike if a single frame is equal to ten
+                score += MAX_PINS_PER_FRAME + rolls[bowls + 1] + rolls[bowls + 2];
                 bowls++;
-            }else if (rolls[bowls] + rolls[bowls + 1] == 10) { // Should check for a Spare
-                score += 10 + rolls[bowls + 2]; // increments score by 10 plus whatever next frame equals
+            }else if (rolls[bowls] + rolls[bowls + 1] == MAX_PINS_PER_FRAME) { // Should check for a Spare
+                score += MAX_PINS_PER_FRAME + rolls[bowls + 2]; // increments score by 10 plus whatever next frame equals
                 bowls += 2;
             } else { // If no spare
-                score += rolls[bowls] + rolls[bowls + 1]; // increment score by bowls value
+                score += rolls[bowls] + rolls[bowls + 1]; // increment score by bowls value each frame has 2 parts to it.
                 bowls += 2; // continue through bowls
             }
             frame++; // Continue through frames
